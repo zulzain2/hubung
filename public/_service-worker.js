d5b78e366196b6ab673fd48560f8bc4bc035a8d7
ZulZayn <zulwaqarzain96@gmail.com>
2021-05-07 22:03:04 +0800

    sw15

--- public/_service-worker.js
+++ public/_service-worker.js
@@ -36,8 +36,8 @@
 
 
 
-var CACHE_STATIC_NAME = 'static-v14';
-var CACHE_DYNAMIC_NAME = 'dynamic-v14';
+var CACHE_STATIC_NAME = 'static-v15';
+var CACHE_DYNAMIC_NAME = 'dynamic-v15';
 var STATIC_FILES = [
     '/offline',
     '/home',
