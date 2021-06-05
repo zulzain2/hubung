b6ccb3cd1ac751cfbf9f76ff0db2150b5f1df4cc
ZulZayn <zulwaqarzain96@gmail.com>
2021-06-06 06:47:31 +0800

    socket chat

--- socket/socket-chat/server.js
+++ socket/socket-chat/server.js
@@ -4,7 +4,12 @@ const socketio = require('socket.io');
 const fetch = require('node-fetch'); 
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app , (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Socket Chat Works!\n');
+  });
+
 const io = socketio(server, {
     cors: {
       origin: "*",
