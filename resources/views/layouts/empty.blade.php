714ea967b6c393f659080d08ecda5c27ee730bb8
ZulZayn <zulwaqarzain96@gmail.com>
2021-06-09 20:53:54 +0800

    empty id user

--- resources/views/layouts/empty.blade.php
+++ resources/views/layouts/empty.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="domain" content="{{ URL::to('') }}">
+    <meta name="id_user" content="">
     <meta name="theme-color" content="#000">
     <title>Hubung</title>
 
